275852f64022434720ea0a3540db81cda30bfa50
sebastian1028 <juansebastian.proyecto11@gmail.com>
2026-08-09 16:15:05 -0500

    titulo mas descriptivo en la pestana del navegador

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = dash.Dash(
     __name__,
     meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
 )
-app.title = "Dashboard energia"
+app.title = "Pronóstico de producción energética - Austria"
 
 server = app.server
 app.config.suppress_callback_exceptions = True
